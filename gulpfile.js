71f8df60548d436131ec7e50f4eb6e3a5a222a55
mtorres <migueltorres@orange.fr>
2017-08-17 16:52:50 +0200

    Edit gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,21 @@
 const gulp = require('gulp');
 
-gulp.task('default', function(){
-    //code for tasking
+//return all html files into all app directories
+gulp.task('html', function(){
+    return gulp.src(['./app/*.html', './app/**/*.html'])
+        .pipe(gulp.dest('./dist'));
 });
+
+//return all scripts
+gulp.task('scripts', function(){
+    return gulp.src(['./app/scripts/*.js', './app/scripts/**/*.js'])
+        .pipe(gulp.dest('./dist/js'));
+});
+
+//return stylesheets
+gulp.task('styles', function(){
+    return gulp.src('./app/css/**/*.css')
+        .pipe(gulp.dest('./dist/css'));
+});
+
+gulp.task('default', ['html', 'scripts', 'styles']);
